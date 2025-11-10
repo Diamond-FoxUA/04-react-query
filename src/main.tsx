8fe2f5b2312478d4a5c3fe48bd238767bdb82dc4
Dmytro Farbun <dimofarb@gmail.com>
2025-11-10 18:22:27 +0200

    assets: install react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './components/App/App'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+const queryClient = new QueryClient();
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <QueryClientProvider client={queryClient}>
     <App />
-  </StrictMode>,
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>,
 )
